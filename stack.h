86db223d996582930542ce753ed5325c13d35a68
SzymonKozl <szymon.kozlowski.2003@gmail.com>
2023-12-04 13:34:01 +0100

    declarations (might change in future)

---
+++ stack.h
@@ -0,0 +1,34 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include <utility>
+#include <map>
+#include <vector>
+#include <memory>
+
+
+namespace cxx {
+    template<typename K, typename V> class stack {
+        public:
+            stack() noexcept;
+            stack(stack const &) noexcept;
+            stack(stack &&);
+            stack& operator=(stack);
+            void push(K const &, V const &);
+            void pop();
+            void pop(K const &);
+            std::pair<K const &, V &> front();
+            std::pair<K const &, V const &> front() const;
+            size_t size() const;
+            size_t count(K const &) const;
+            void clear();
+        private:
+            bool is_shallow_copy;
+            std::shared_ptr<std::map<K, std::vector<V>>> stacks;
+            std::shared_ptr<std::vector<V>> main_stack;
+    };
+}
+
+
+
+#endif
